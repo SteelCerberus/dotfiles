cfb9e7fcb0267e96a5cafc6b683963a220697a26
SteelCerberus <cerberus.growler@gmail.com>
2025-08-15 17:49:35 -0400

    CachyOS fastfetch update

--- .config/fish/functions/c.fish
+++ .config/fish/functions/c.fish
@@ -2,7 +2,6 @@
 
 function c -d "Clear the screen and display fastfetch"
     clear
-    fastfetch --config ~/.config/fastfetch/config_small.jsonc
-    echo
+    kitten icat -n --place 19x19@0x0 --scale-up --align left "$HOME/.config/fastfetch/logos/cachy1.png" | fastfetch --logo-width 20 -c "$HOME/.config/fastfetch/config_small.jsonc" --raw -
 end
 
--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
-    fastfetch --config ~/.config/fastfetch/config_small.jsonc
+    kitten icat -n --place 19x19@0x0 --scale-up --align left "$HOME/.config/fastfetch/logos/cachy1.png" | fastfetch --logo-width 20 -c "$HOME/.config/fastfetch/config_small.jsonc" --raw -
     echo
 end
